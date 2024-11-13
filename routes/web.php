03011519961c8b7f6e191727c37babdfac9a53ac
Esmatullah Hadi <esmatullahahdi@gmail.com>
2024-11-13 15:50:07 +0430

    Practice more on route and blade template

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,79 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view("index", [
-        "name"=>"Esmatullah",
-    ]);
-});
+class Task
+{
+  public function __construct(
+    public int $id,
+    public string $title,
+    public string $description,
+    public ?string $long_description,
+    public bool $completed,
+    public string $created_at,
+    public string $updated_at
+  ) {
+  }
+}
 
-Route::get("/{variable}", function ($variable) {
-    return view("index", [
-        "name"=>$variable,
-    ]);
-});
+$tasks = [
+  new Task(
+    1,
+    'Buy groceries',
+    'Task 1 description',
+    'Task 1 long description',
+    false,
+    '2023-03-01 12:00:00',
+    '2023-03-01 12:00:00'
+  ),
+  new Task(
+    2,
+    'Sell old stuff',
+    'Task 2 description',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+  ),
+  new Task(
+    3,
+    'Learn programming',
+    'Task 3 description',
+    'Task 3 long description',
+    true,
+    '2023-03-03 12:00:00',
+    '2023-03-03 12:00:00'
+  ),
+  new Task(
+    4,
+    'Take dogs for a walk',
+    'Task 4 description',
+    null,
+    false,
+    '2023-03-04 12:00:00',
+    '2023-03-04 12:00:00'
+  ),
+];
 
-Route::get("/hello", function() {
-    return "Hello from here!";
-});
+Route::get("/", function() {
+  return view("index");
+})->name("tasks.index");
 
-Route::get("/hello/{name}", function($name) {
-    return "Greetings to my best friend ". $name . "!";
-}); 
+// Route::get("/", function() {
+//   return redirect()->route("tasks.index");
+// });
 
-Route::fallback(function() {
-    return "Hey don't worry! I got you.";
-});
+// Route::get("/tasks", function() use($tasks) {
+//   return view("check", ["tasks"=>$tasks]);
+// })->name("tasks.main");
 
-Route::get("/{work}", function($url) {
-    return "your site is ". $url;
-});
+Route::get("/tasks/{id}", function ($id) use($tasks) {
+  $task = collect($tasks)->firstWhere("id", $id);
+  if (!$task) {
+    abort(Response::HTTP_NOT_FOUND);
+  };
+  return view("show", ["task"=>$task]);
+})->name("tasks.show");
+
+?>
