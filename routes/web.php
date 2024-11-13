d17a6f113fba4c1e2f3631f3c8a2cd7e2e67efb4
Esmatullah Hadi <esmatullahahdi@gmail.com>
2024-11-13 16:13:57 +0430

    connect laravel app to database

--- routes/web.php
+++ routes/web.php
@@ -60,13 +60,6 @@ Route::get("/", function() {
   return view("index");
 })->name("tasks.index");
 
-// Route::get("/", function() {
-//   return redirect()->route("tasks.index");
-// });
-
-// Route::get("/tasks", function() use($tasks) {
-//   return view("check", ["tasks"=>$tasks]);
-// })->name("tasks.main");
 
 Route::get("/tasks/{id}", function ($id) use($tasks) {
   $task = collect($tasks)->firstWhere("id", $id);
